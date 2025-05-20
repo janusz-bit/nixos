585cf20da28e53eb48e91dfcb21da3ac4017a37a
janusz-bit <antyphone@gmail.com>
2025-05-20 20:33:12 +0200

    Update STEAM_EXTRA_COMPAT_TOOLS_PATHS

--- configuration.nix
+++ configuration.nix
@@ -136,7 +136,7 @@
   };
 
   environment.sessionVariables = {
-    STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS = "~/.steam/root/compatibilitytools.d";
   };
 
   # Some programs need SUID wrappers, can be configured further or are
