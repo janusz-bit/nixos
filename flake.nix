f88bb4580b92505bfede571e698591e2a5e05d60
fatmichealcoder <fatmichealcoder@gmail.com>
2025-07-25 21:38:18 +0200

    Update nixpkgs reference to version 25.05 in flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     g14_patches = {
       url = "github:CachyOS/kernel-patches";
