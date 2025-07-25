ac70033c15126dc4db6cae30e7c1d429da07edb9
fatmichealcoder <fatmichealcoder@gmail.com>
2025-07-25 23:27:52 +0200

    Update nixpkgs reference to use the latest unstable version in flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     g14_patches = {
       url = "github:CachyOS/kernel-patches";
