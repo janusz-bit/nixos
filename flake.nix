dc6a89c6e9d8bd6a49c00d2ee0c711495bc6d62d
janusz-bit <antyphone@gmail.com>
2025-09-11 08:53:30 +0200

    Custom ISO with `nix build`

--- flake.nix
+++ flake.nix
@@ -6,6 +6,10 @@
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     nix-alien.url = "github:thiagokokada/nix-alien";
     chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
@@ -14,6 +18,7 @@
       nixpkgs,
       nixos-hardware,
       chaotic,
+      nixos-generators,
       ...
     }@inputs:
     {
@@ -26,6 +31,33 @@
           chaotic.nixosModules.default # IMPORTANT
           nixos-hardware.nixosModules.asus-fa507nv
         ];
+      };
+
+      packages.x86_64-linux = {
+        customISO = nixos-generators.nixosGenerate {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs; };
+          modules = [
+            # you can include your own nixos configuration here, i.e.
+            # ./configuration.nix
+            ./configuration.nix
+            chaotic.nixosModules.default # IMPORTANT
+            nixos-hardware.nixosModules.asus-fa507nv
+          ];
+          format = "install-iso";
+          
+
+          # optional arguments:
+          # explicit nixpkgs and lib:
+          # pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          # lib = nixpkgs.legacyPackages.x86_64-linux.lib;
+          # additional arguments to pass to modules:
+          # specialArgs = { myExtraArg = "foobar"; };
+
+          # you can also define your own custom formats
+          # customFormats = { "myFormat" = <myFormatModule>; ... };
+          # format = "myFormat";
+        };
       };
 
     };
