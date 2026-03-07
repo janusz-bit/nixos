b4f85bb9bb34b28eb83e0c32f77de7f076d854b2
janusz-bit <janusz-bit@proton.me>
2026-03-07 05:11:23 +0000

    Default editor zed

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -2,6 +2,9 @@
 {
   flake.nixosModules.configuration =
     { pkgs, ... }:
+    let
+      editor = "zeditor --wait";
+    in
     {
       nixpkgs.config.allowUnfree = true;
 
@@ -14,7 +17,8 @@
       ];
 
       environment.sessionVariables.NIXOS_OZONE_WL = "1";
-
+      environment.sessionVariables.VISUAL = editor;
+      environment.sessionVariables.EDITOR = editor;
       nix.settings.experimental-features = [
         "nix-command"
         "flakes"
