a85da47bc6e8d15483f041975c08efc139a806f0
janusz-bit <janusz-bit@proton.me>
2026-03-11 04:41:14 +0000

    Fix system for avf-android

--- modules/hosts/avf-android/default.nix
+++ modules/hosts/avf-android/default.nix
@@ -1,7 +1,7 @@
 { inputs, self, ... }:
 {
   flake.nixosConfigurations.avf = inputs.nixpkgs.lib.nixosSystem {
-    system = "x86_64-linux";
+    system = "aarch64-linux";
     modules = [
       self.nixosModules.base
       inputs.avf.nixosModules.avf
