9602ac759c683ad3dd6b0eb1bab27c503083766c
janusz-bit <janusz-bit@proton.me>
2026-03-11 01:39:58 +0000

    Turn off obsidian from wsl

--- modules/hosts/wsl/wsl.nix
+++ modules/hosts/wsl/wsl.nix
@@ -5,7 +5,7 @@
     {
       imports = [
         self.nixosModules.wsl-stateVersion
-        self.nixosModules.wsl-obsidian
+        # self.nixosModules.wsl-obsidian
 
         inputs.nixos-wsl.nixosModules.default
         self.nixosModules.wsl-settings
