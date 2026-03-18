d6bbc266a29530e1a2e93a0a3e9d8f8aa79d152e
janusz-bit <janusz-bit@proton.me>
2026-03-18 16:17:06 +0000

    Fix for wsl "Enter passphrase" in ssh

--- modules/hosts/ssh.nix
+++ modules/hosts/ssh.nix
@@ -9,10 +9,6 @@
         settings.PasswordAuthentication = false;
         settings.KbdInteractiveAuthentication = false;
       };
-      # By default, KDE prompts you to enter the passwords for your SSH keys to unlock them across session starts. To avoid being asked to unlock your SSH keys every time a session is restarted (e.g., after logging out or rebooting), you can use ksshaskpass to store the passwords.
-      environment.variables = {
-        SSH_ASKPASS_REQUIRE = "prefer";
-      };
 
       programs.ssh.startAgent = true;
       programs.ssh.enableAskPassword = true;
