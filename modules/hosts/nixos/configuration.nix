a83b31fa2081a9fbdb4ef3b096aa5c1c80551b9e
janusz-bit <janusz-bit@proton.me>
2026-03-18 05:49:07 +0100

    Refactoring

--- modules/hosts/nixos/configuration.nix
+++ modules/hosts/nixos/configuration.nix
@@ -9,9 +9,6 @@
     }:
 
     {
-      # Bootloader.
-      # boot.loader.systemd-boot.enable = true;
-      #
       specialisation = {
         reverse-sync.configuration = {
           hardware.nvidia = {
@@ -88,16 +85,11 @@
 
       };
       hardware = {
-
         bluetooth.enable = true;
       };
 
       networking.hostName = "nixos"; # Define your hostname.
       # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-      # Configure network proxy if necessary
-      # networking.proxy.default = "http://user:password@proxy:port/";
-      # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
       # Enable networking
       networking.networkmanager.enable = true;
@@ -124,9 +116,6 @@
       console.keyMap = "pl2";
       security.rtkit.enable = true;
 
-      # Enable touchpad support (enabled default in most desktopManager).
-      # services.xserver.libinput.enable = true;
-
       # Define a user account. Don't forget to set a password with ‘passwd’.
       users.users.dinosaur = {
         isNormalUser = true;
@@ -136,44 +125,10 @@
           "wheel"
           "gamemode"
         ];
-        packages = with pkgs; [
-          # kdePackages.kate
-          #  thunderbird
-          # kdePackages.bluedevil
-          # kdePackages.bluez-qt
-          # openobex
-          # obexftp
-        ];
       };
-
       # Allow unfree packages
       nixpkgs.config.allowUnfree = true;
 
-      # List packages installed in system profile. To search, run:
-      # $ nix search wget
-      environment.systemPackages = with pkgs; [
-        #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-        #  wget
-      ];
-
-      # Some programs need SUID wrappers, can be configured further or are
-      # started in user sessions.
-      # programs.mtr.enable = true;
-      # programs.gnupg.agent = {
-      #   enable = true;
-      #   enableSSHSupport = true;
-      # };
-
-      # List services that you want to enable:
-
-      # Enable the OpenSSH daemon.
-      # services.openssh.enable = true;
-
-      # Open ports in the firewall.
-      # networking.firewall.allowedTCPPorts = [ ... ];
-      # networking.firewall.allowedUDPPorts = [ ... ];
-      # Or disable the firewall altogether.
-      # networking.firewall.enable = false;
     };
 
 }
