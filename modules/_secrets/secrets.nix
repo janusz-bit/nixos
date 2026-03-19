237720a3f1279902429d801757d7df28df4a16d0
janusz-bit <janusz-bit@proton.me>
2026-03-19 20:04:50 +0100

    Added wsl key

--- modules/_secrets/secrets.nix
+++ modules/_secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   nixos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHrOn7Xvw90cBH1EJiFOR6r3RHXRsbFLm0Wpi6H+db+o root@nixos
 ";
-  wsl = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGq10J7I5kid1/2R3OZFGAqE9BDqYp1h98nHuBg5KylF root@nixos";
+  wsl = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIb1ln2lU/lR4NwlfUQ+oPurNDI+O6B0uiFCcWfYuGj3 root@nixos";
 in
 {
   "secret1.age" = {
