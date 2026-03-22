f331597b19f5fc4e87fef97df83b6066230e1f86
janusz-bit <janusz-bit@proton.me>
2026-03-22 11:10:54 +0100

    Update packages.nix

--- modules/packages/packages.nix
+++ modules/packages/packages.nix
@@ -4,7 +4,7 @@
     { pkgs, ... }:
     {
       packages.proton-cachyos-v3 = (
-        pkgs.callPackage ./modules/packages/proton-bin {
+        pkgs.callPackage ./_proton-bin {
           toolTitle = "Proton-CachyOS x86-64-v3";
           tarballPrefix = "proton-";
           tarballSuffix = "-x86_64_v3.tar.xz";
