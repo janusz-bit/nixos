11fc637a86d68bd118de80452da643d98ebd4b76
janusz-bit <janusz-bit@proton.me>
2026-03-28 11:26:45 +0100

    Update install.nix

--- modules/packages/install.nix
+++ modules/packages/install.nix
@@ -2,12 +2,17 @@
 {
   perSystem =
     { pkgs, ... }:
+    let
+      text = ''
+        sudo nix run github:nix-community/disko  --experimental-features 'nix-command flakes' -- --mode destroy,format,mount --flake github:janusz-bit/nixos#nixos
+        sudo nixos-install --flake github:janusz-bit/nixos#nixos --no-root-passwd
+      '';
+    in
     {
       packages.install-system = pkgs.writeShellScriptBin "install-system" ''
         #!/usr/bin/env bash
         set -e
-        sudo nix run github:nix-community/disko -- --mode destroy,format,mount --flake github:janusz-bit/nixos#nixos
-        sudo nixos-install --flake github:janusz-bit/nixos#nixos --no-root-passwd
+        echo "${text}"
       '';
     };
 }
