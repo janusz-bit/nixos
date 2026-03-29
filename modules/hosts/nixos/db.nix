e08cdc289bf7b7326e72bfc73b953d5f4660214b
janusz-bit <janusz-bit@proton.me>
2026-03-29 14:30:22 +0200

    Oracle database

---
+++ modules/hosts/nixos/db.nix
@@ -0,0 +1,32 @@
+{ inputs, ... }:
+{
+  flake.nixosModules.nixos-db =
+    {
+      config,
+      pkgs,
+      lib,
+      ...
+    }:
+
+    {
+      virtualisation = {
+        containers.enable = true;
+        podman = {
+          enable = true;
+          dockerCompat = true;
+          defaultNetwork.settings.dns_enabled = true; # Required for containers under podman-compose to be able to talk to each other.
+        };
+      };
+
+      users.users.dinosaur = {
+        # replace `<USERNAME>` with the actual username
+        extraGroups = [
+          "podman"
+        ];
+      };
+      environment.systemPackages = [
+        pkgs.oracle-instantclient
+      ];
+    };
+
+}
