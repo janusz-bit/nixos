464c36a538ad8c270c641a33679bec9502b27dd9
janusz-bit <janusz-bit@proton.me>
2026-04-02 16:51:57 +0200

    Update secrets.nix

--- modules/_secrets/secrets.nix
+++ modules/_secrets/secrets.nix
@@ -1,19 +1,19 @@
 let
-  nixos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE91CHTRZUuVVgHW8BJkT9rYEkYYBFR3YpjGxW0tRdzq root@nixos";
-  wsl = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIb1ln2lU/lR4NwlfUQ+oPurNDI+O6B0uiFCcWfYuGj3 root@nixos";
+  nixos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKVh4xGoawFG0akl0mh34/kUDvzC8NJ/wxGgAMF7rqzm root@nixos";
+  droid-android = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIb1ln2lU/lR4NwlfUQ+oPurNDI+O6B0uiFCcWfYuGj3 root@nixos";
 in
 {
   "secret1.age" = {
     publicKeys = [
       nixos
-      wsl
+      droid-android
     ];
     armor = true;
   };
   "notes.age" = {
     publicKeys = [
       nixos
-      wsl
+      droid-android
     ];
     armor = true;
   };
