53e8ff52a2c5e3b332e331dba2371850481b8441
janusz-bit <janusz-bit@proton.me>
2026-04-02 17:02:40 +0200

    Update install.nix

--- modules/packages/install.nix
+++ modules/packages/install.nix
@@ -5,7 +5,7 @@
     let
       text = ''
         sudo nix run github:nix-community/disko  --experimental-features 'nix-command flakes' -- --mode destroy,format,mount --flake github:janusz-bit/nixos#nixos
-        sudo nixos-install --flake github:janusz-bit/nixos#nixos --no-root-passwd --option extra-substituters "https://attic.xuyh0120.win/lantian" --option  extra-trusted-public-keys "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
+        sudo nixos-install --flake github:janusz-bit/nixos#nixos --no-root-passwd --option extra-substituters "https://attic.xuyh0120.win/lantian https://janusz-bit.cachix.org" --option  extra-trusted-public-keys "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc= janusz-bit.cachix.org-1:4stTiufAF02BAXw8HNvYslAmUlPbZPIRhIGht0gSMoo="
       '';
     in
     {
