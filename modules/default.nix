b09679ee03363ac287b39264ebe016f69abf49b2
janusz-bit <janusz-bit@proton.me>
2026-04-03 15:00:53 +0200

    Default package = install-system

--- modules/default.nix
+++ modules/default.nix
@@ -8,8 +8,9 @@
   imports = [ inputs.home-manager.flakeModules.home-manager ];
 
   perSystem =
-    { pkgs, ... }:
+    { pkgs, self', ... }:
     {
       formatter = pkgs.nixfmt-tree;
+      packages.default = self'.packages.install-system;
     };
 }
