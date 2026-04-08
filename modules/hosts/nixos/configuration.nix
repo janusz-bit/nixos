e0284ea60265b51dc14cc55824a210485bb23d95
janusz-bit <janusz-bit@proton.me>
2026-04-08 03:59:19 +0200

    refactor: apply bootdev-cli overlay globally

--- modules/hosts/nixos/configuration.nix
+++ modules/hosts/nixos/configuration.nix
@@ -50,7 +50,10 @@
           };
         };
       };
-      nixpkgs.overlays = [ inputs.nix-cachyos-kernel.overlays.default ];
+      nixpkgs.overlays = [
+        inputs.nix-cachyos-kernel.overlays.default
+        inputs.self.overlays.bootdev-cli-overlay
+      ];
       boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest-lto-x86_64-v3;
       environment.sessionVariables = {
         GAMEMODERUNEXEC = "env __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia __VK_LAYER_NV_optimus=NVIDIA_only PROTON_ENABLE_WAYLAND=1 PROTON_FSR4_UPGRADE=1	PROTON_DLSS_UPGRADE=1 PROTON_XESS_UPGRADE=1 PROTON_USE_NTSYNC=1";
--- modules/hosts/nixos/packages.nix
+++ modules/hosts/nixos/packages.nix
@@ -24,26 +24,7 @@
         element-desktop
         prismlauncher
         lutris
-        (bootdev-cli.overrideAttrs (oldAttrs: rec {
-          # Nadpisujemy wersję (np. na najnowszą wydaną lub commit)
-          version = "1.28.0"; # Wpisz tutaj żądaną wersję
-
-          # Pobieramy nowy kod źródłowy z GitHuba
-          src = fetchFromGitHub {
-            owner = "bootdotdev";
-            repo = "bootdev";
-            rev = "v${version}"; # lub użyj pełnego hasha commita
-            # Hash dla nowego kodu źródłowego. Przy pierwszej próbie
-            # budowania Nix zgłosi błąd i poda prawidłowy hash - wtedy go tu wklej.
-            hash = "sha256-sBPId1wEsIG1E+sf+pbqfz0xW0+PHVAoRYTkFLXpWOU=";
-          };
-
-          # Aplikacje w Go wymagają weryfikacji modułów (zależności)
-          # Przy zmianie kodu, vendorHash prawie na pewno ulegnie zmianie.
-          # Ustaw na pusty, aby Nix wyrzucił błąd i podał nowy hash.
-          vendorHash = "sha256-ZDioEU5uPCkd+kC83cLlpgzyOsnpj2S7N+lQgsQb8uY=";
-          # vendorHash = "sha256-BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB=";
-        }))
+        bootdev-cli
       ];
       # Install firefox.
       programs.firefox.enable = true;
