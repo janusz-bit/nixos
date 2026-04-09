20e3bea7f05e8a18df4db50b0577c0db0c30c644
janusz-bit <janusz-bit@proton.me>
2026-04-09 19:24:03 +0200

    Fix for template

--- modules/templates/_project.nix/flake.nix
+++ modules/templates/_project.nix/flake.nix
@@ -5,6 +5,10 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
+    git-hooks-nix = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
