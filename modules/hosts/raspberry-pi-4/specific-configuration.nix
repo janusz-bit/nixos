92b9a1e81059a9fa972a8359e2fa55781529077d
janusz-bit <janusz-bit@proton.me>
2026-04-12 03:13:19 +0200

    Removed hardware.deviceTree from my config (conflict with nixos-hardware) raspberry-pi-4

--- modules/hosts/raspberry-pi-4/specific-configuration.nix
+++ modules/hosts/raspberry-pi-4/specific-configuration.nix
@@ -5,10 +5,6 @@
     {
       hardware = {
         raspberry-pi."4".apply-overlays-dtmerge.enable = true;
-        deviceTree = {
-          enable = true;
-          filter = "*rpi-4-*.dtb";
-        };
       };
       console.enable = false;
       environment.systemPackages = with pkgs; [
