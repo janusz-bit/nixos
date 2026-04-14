32a3e52e0bedee0a02ed10432624f8e6835b299c
janusz-bit <janusz-bit@proton.me>
2026-04-14 05:13:16 +0200

    Update raspberry-pi-4.nix

--- modules/hosts/raspberry-pi-4/raspberry-pi-4.nix
+++ modules/hosts/raspberry-pi-4/raspberry-pi-4.nix
@@ -7,7 +7,7 @@
         self.nixosModules.raspberry-pi-4-hardware-configuration
         self.nixosModules.raspberry-pi-4-specific
         self.nixosModules.raspberry-pi-4-configuration
-        # inputs.nixos-hardware.nixosModules.raspberry-pi-4
+        inputs.nixos-hardware.nixosModules.raspberry-pi-4
         # self.nixosModules.base
         self.nixosModules.options
         (_: {
