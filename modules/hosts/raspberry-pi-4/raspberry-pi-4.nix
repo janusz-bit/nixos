cf78d75d681c0bfe820ffadfa90a305ff3176858
janusz-bit <janusz-bit@proton.me>
2026-04-16 21:14:42 +0200

    Update raspberry-pi-4.nix

--- modules/hosts/raspberry-pi-4/raspberry-pi-4.nix
+++ modules/hosts/raspberry-pi-4/raspberry-pi-4.nix
@@ -10,8 +10,7 @@
         self.nixosModules.agenix
         self.nixosModules.raspberry-pi-4-specific
         self.nixosModules.raspberry-pi-4-configuration
-        inputs.nixos-raspberrypi.nixosModules.raspberry-pi-4
-        # self.nixosModules.base
+        inputs.nixos-hardware.nixosModules.raspberry-pi-4
         self.nixosModules.options
         (_: {
           custom.flakeTarget = "raspberry-pi-4";
