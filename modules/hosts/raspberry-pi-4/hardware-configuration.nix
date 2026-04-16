d88f2600a0c89e3b030163123654937f8cda8556
janusz-bit <janusz-bit@proton.me>
2026-04-16 07:25:01 +0200

    XD

--- modules/hosts/raspberry-pi-4/hardware-configuration.nix
+++
@@ -1,4 +0,0 @@
-_: {
-  flake.nixosModules.raspberry-pi-4-hardware-configuration = import ./_hardware-configuration/hardware-configuration.nix;
-
-}
--- modules/hosts/raspberry-pi-4/specific.nix
+++
@@ -1,5 +0,0 @@
-_: {
-  flake.nixosModules.raspberry-pi-4-specific = _: {
-    system.stateVersion = "26.05";
-  };
-}
