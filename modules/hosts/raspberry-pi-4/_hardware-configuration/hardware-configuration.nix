2c244f6a0b33d98a33f63d42b09d681869688c16
janusz-bit <janusz-bit@proton.me>
2026-04-17 04:19:43 +0200

    Update hardware-configuration.nix

--- modules/hosts/raspberry-pi-4/_hardware-configuration/hardware-configuration.nix
+++ modules/hosts/raspberry-pi-4/_hardware-configuration/hardware-configuration.nix
@@ -14,6 +14,11 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
+    fsType = "ext4";
+  };
+
   boot.initrd.availableKernelModules = [ "xhci_pci" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
