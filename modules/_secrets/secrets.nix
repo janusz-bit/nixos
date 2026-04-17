4954454b078564a7e852632987213acfe8d176dc
janusz-bit <janusz-bit@proton.me>
2026-04-17 07:31:56 +0200

    Added raspberry-pi-4 to agenix

--- modules/_secrets/secrets.nix
+++ modules/_secrets/secrets.nix
@@ -1,12 +1,14 @@
 let
   nixos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOcA/U1pLJuoQsS//IQ264xwbz5c9E+Yc+lZBZ/NavUm root@nixos";
   droid-android = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIb1ln2lU/lR4NwlfUQ+oPurNDI+O6B0uiFCcWfYuGj3 root@nixos";
+  raspberry-pi-4 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPgUBv6niliD4hpn9G8ngDZIFive/1qyVN9TV7S8pk9v root@raspberry-pi-4";
 in
 {
   "secret1.age" = {
     publicKeys = [
       nixos
       droid-android
+      raspberry-pi-4
     ];
     armor = true;
   };
@@ -14,6 +16,7 @@ in
     publicKeys = [
       nixos
       droid-android
+      raspberry-pi-4
     ];
     armor = true;
   };
@@ -21,6 +24,7 @@ in
     publicKeys = [
       nixos
       droid-android
+      raspberry-pi-4
     ];
     armor = true;
   };
