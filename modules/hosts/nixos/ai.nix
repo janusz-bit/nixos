c5d85a22f56b66d2885c3271f55b1106c9b77698
janusz-bit <janusz-bit@proton.me>
2026-04-18 23:27:02 +0200

    Update ai.nix

--- modules/hosts/nixos/ai.nix
+++ modules/hosts/nixos/ai.nix
@@ -8,9 +8,8 @@
       # services.open-webui.enable = true;
 
       environment.systemPackages = with pkgs; [
-        python3
-        python3Packages.pip
-        python3Packages.aiohttp
+        uv
+        repomix
       ];
     };
 }
