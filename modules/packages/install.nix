af23a34ca6a6614a74579a1719ed9a99e0da91fb
Janusz Janusz <janusz-bit@proton.me>
2026-04-20 00:07:14 +0200

    Update install.nix

--- modules/packages/install.nix
+++ modules/packages/install.nix
@@ -7,7 +7,7 @@
         sudo ${lib.getExe pkgs.disko} --mode destroy,format,mount --flake ${custom.repository.linkFlake}#nixos
         sudo mkdir -p /mnt/etc/nixos
         sudo ${pkgs.git}/bin/git clone ${custom.repository.url} /mnt/etc/nixos
-        sudo ${pkgs.nixos-install-tools}/bin/nixos-install --flake /mnt/etc/nixos#nixos --no-root-passwd --accept-flake-config
+        sudo ${pkgs.nixos-install-tools}/bin/nixos-install --flake /mnt/etc/nixos#nixos --no-root-passwd
       '';
     in
     {
