19f05ca4e3b777d2fab4b72e066f84e8819881a0
janusz-bit <janusz-bit@proton.me>
2026-04-21 01:50:28 +0200

    Added custom setting for raspberry-pi-4-sd-image

--- modules/hosts/raspberry-pi-4/default.nix
+++ modules/hosts/raspberry-pi-4/default.nix
@@ -21,6 +21,12 @@
       { nixpkgs.hostPlatform = "aarch64-linux"; }
       self.nixosModules."raspberry-pi-4"
       "${inputs.nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+      {
+        sdImage = {
+          expandOnBoot = true;
+          compressImage = false;
+        };
+      }
     ];
   };
 }
