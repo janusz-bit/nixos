93321590edf495fb5347d87f552508449a45799c
janusz-bit <janusz-bit@proton.me>
2026-04-21 03:59:06 +0200

    Update nextcloud.nix

--- modules/hosts/raspberry-pi-4/nextcloud.nix
+++ modules/hosts/raspberry-pi-4/nextcloud.nix
@@ -53,10 +53,10 @@
         80
         443
       ];
-      services.nextcloud.config.extraTrustedDomains = [ "localhost" ];
       services.nextcloud.settings = {
         overwriteprotocol = "https";
         trusted_proxies = [ "127.0.0.1" ];
+        trusted_domains = [ "localhost" ];
       };
     };
 }
