6c86a370927512bc780dcca07767f763411151bd
janusz-bit <janusz-bit@proton.me>
2026-04-21 02:28:28 +0200

    Update secrets.nix

--- modules/_secrets/secrets.nix
+++ modules/_secrets/secrets.nix
@@ -1,5 +1,5 @@
 let
-  nixos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOcA/U1pLJuoQsS//IQ264xwbz5c9E+Yc+lZBZ/NavUm root@nixos";
+  nixos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAkQRhJASMQB1ClDBwqnYGZXSSGAr1S2y5KaQ5Z0Fc5+ root@nixos";
   droid-android = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIb1ln2lU/lR4NwlfUQ+oPurNDI+O6B0uiFCcWfYuGj3 root@nixos";
   raspberry-pi-4 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPgUBv6niliD4hpn9G8ngDZIFive/1qyVN9TV7S8pk9v root@raspberry-pi-4";
 in
