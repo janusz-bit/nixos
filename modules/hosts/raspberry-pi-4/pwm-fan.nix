0bd03971fd4e21095917190f1531d86421d5e330
janusz-bit <janusz-bit@proton.me>
2026-04-24 06:40:15 +0200

    Added module "raspberry-pi-4/pwm-fan"

---
+++ modules/hosts/raspberry-pi-4/pwm-fan.nix
@@ -0,0 +1,62 @@
+{ self, ... }:
+{
+  flake.nixosModules."raspberry-pi-4/pwm-fan" =
+    {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
+    {
+      systemd.services.pwm-fan = {
+        description = "Waveshare PWM Fan Control";
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig = {
+          Type = "simple";
+          Restart = "always";
+          RestartSec = "10";
+          User = "root";
+        };
+        script =
+          let
+            pythonEnv = pkgs.python3.withPackages (ps: [ ps.RPi_GPIO ]);
+          in
+          ''
+            ${pythonEnv}/bin/python -c '
+            import RPi.GPIO as GPIO
+            import time
+
+            # GPIO 14 (BCM) odpowiada fizycznemu pinowi 8 (TXD) widocznemu na zdjęciu
+            FAN_PIN = 14
+
+            GPIO.setwarnings(False)
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(FAN_PIN, GPIO.OUT)
+            pwm = GPIO.PWM(FAN_PIN, 50)
+            pwm.start(0)
+
+            def get_temp():
+                try:
+                    with open("/sys/class/thermal/thermal_zone0/temp", "r") as f:
+                        return float(f.read()) / 1000.0
+                except:
+                    return 0.0
+
+            try:
+                while True:
+                    temp = get_temp()
+                    if temp >= 60.0:
+                        pwm.ChangeDutyCycle(100)
+                    elif temp >= 48.0:
+                        pwm.ChangeDutyCycle(50)
+                    else:
+                        pwm.ChangeDutyCycle(0)
+                    time.sleep(5)
+            except BaseException:
+                pwm.stop()
+                GPIO.cleanup()
+            '
+          '';
+      };
+    };
+}
--- modules/hosts/raspberry-pi-4/raspberry-pi-4.nix
+++ modules/hosts/raspberry-pi-4/raspberry-pi-4.nix
@@ -6,6 +6,7 @@
       imports = [
         self.nixosModules."raspberry-pi-4/nextcloud"
         self.nixosModules."raspberry-pi-4/cloudflared"
+        self.nixosModules."raspberry-pi-4/pwm-fan"
         self.nixosModules."agenix"
         self.nixosModules."raspberry-pi-4/specific"
         self.nixosModules."raspberry-pi-4/configuration"
