6da9964bff4d52f135cb846ea6de305ed629ef34
janusz-bit <janusz-bit@proton.me>
2026-04-30 00:47:22 +0200

    Update ai.nix

--- modules/hosts/nixos/ai.nix
+++ modules/hosts/nixos/ai.nix
@@ -7,7 +7,11 @@
       services.ollama.package = pkgs.ollama-cuda;
       services.open-webui = {
         enable = true;
-        environment = "{\n  OLLAMA_API_BASE_URL = \"http://127.0.0.1:11434\";\n  # Disable authentication\n  WEBUI_AUTH = \"False\";\n}\n";
+        environment = {
+          OLLAMA_API_BASE_URL = "http://127.0.0.1:11434";
+          # Disable authentication
+          WEBUI_AUTH = "False";
+        };
       };
 
       environment.systemPackages = with pkgs; [
