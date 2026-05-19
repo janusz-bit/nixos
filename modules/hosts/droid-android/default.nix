4e28ab2351d7c12c15909552cdb72d95b427e962
janusz-bit <janusz-bit@proton.me>
2026-05-19 02:41:06 +0200

    Formating

--- modules/hosts/droid-android/default.nix
+++ modules/hosts/droid-android/default.nix
@@ -6,20 +6,23 @@
       self.nixosModules."base"
       inputs.avf.nixosModules.avf
       self.nixosModules."droid/stateVersion"
-      ({ lib, ... }: {
-        custom.flakeTarget = "droid";
-        custom.enableFastfetch = false;
-        custom.defaultUser = "droid";
+      (
+        { lib, ... }:
+        {
+          custom.flakeTarget = "droid";
+          custom.enableFastfetch = false;
+          custom.defaultUser = "droid";
 
-        programs.bash.interactiveShellInit = lib.mkBefore ''
-          # Fix for bogus screen size on Android/AVF
-          if [ "$COLUMNS" = "131072" ] || [ "$COLUMNS" = "0" ] || [ -z "$COLUMNS" ]; then
-            export COLUMNS=80
-            export LINES=24
-            stty cols 80 rows 24 2>/dev/null || true
-          fi
-        '';
-      })
+          programs.bash.interactiveShellInit = lib.mkBefore ''
+            # Fix for bogus screen size on Android/AVF
+            if [ "$COLUMNS" = "131072" ] || [ "$COLUMNS" = "0" ] || [ -z "$COLUMNS" ]; then
+              export COLUMNS=80
+              export LINES=24
+              stty cols 80 rows 24 2>/dev/null || true
+            fi
+          '';
+        }
+      )
     ];
   };
 }
