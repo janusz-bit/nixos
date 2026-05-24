bbd611992741286ea4241ccc17495833c1207d0d
janusz-bit <janusz-bit@proton.me>
2026-05-24 09:51:25 +0200

    feat: export OLLAMA_API_KEY environment variable

--- modules/hosts/base/agenix.nix
+++ modules/hosts/base/agenix.nix
@@ -5,6 +5,7 @@
     {
       environment.sessionVariables.CACHIX_AUTH_TOKEN = config.age.secrets.cachix-authtoken.path;
       environment.sessionVariables.GITHUB_TOKEN = config.age.secrets.github-token.path;
+      environment.sessionVariables.OLLAMA_API_KEY = config.age.secrets.ollama-api-key.path;
 
       imports = [ self.nixosModules."agenix" ];
     };
