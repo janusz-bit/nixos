3f684bd204a1d33616e77482eae12c28c97e6d8c
janusz-bit <janusz-bit@proton.me>
2026-05-25 02:22:08 +0200

    Update agenix.nix

--- modules/hosts/base/agenix.nix
+++ modules/hosts/base/agenix.nix
@@ -3,9 +3,9 @@
   flake.nixosModules."base/agenix" =
     { config, pkgs, ... }:
     {
-      environment.sessionVariables.CACHIX_AUTH_TOKEN = "${pkgs.lib.getExe pkgs.cat} ${config.age.secrets.cachix-authtoken.path}";
-      environment.sessionVariables.GITHUB_TOKEN = "${pkgs.lib.getExe pkgs.cat} ${config.age.secrets.github-token.path}";
-      environment.sessionVariables.OLLAMA_API_KEY = "${pkgs.lib.getExe pkgs.cat} ${config.age.secrets.ollama-api-key.path}";
+      environment.sessionVariables.CACHIX_AUTH_TOKEN = "${pkgs.coreutils}/bin/cat ${config.age.secrets.cachix-authtoken.path}";
+      environment.sessionVariables.GITHUB_TOKEN = "${pkgs.coreutils}/bin/cat ${config.age.secrets.github-token.path}";
+      environment.sessionVariables.OLLAMA_API_KEY = "${pkgs.coreutils}/bin/cat ${config.age.secrets.ollama-api-key.path}";
 
       imports = [ self.nixosModules."agenix" ];
     };
