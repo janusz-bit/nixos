50a8a0a22719266bf44fe9de0ab6a1e3482160c6
janusz-bit <janusz-bit@proton.me>
2026-05-29 00:02:57 +0200

    fix(raspberry-pi-4): re-enable Ollama API in Open WebUI

--- modules/hosts/raspberry-pi-4/open-webui.nix
+++ modules/hosts/raspberry-pi-4/open-webui.nix
@@ -11,7 +11,7 @@
           # Point to Hermes Agent API Server (OpenAI-compatible)
           OPENAI_API_BASE_URL = "http://127.0.0.1:8642/v1";
           # Disable Ollama so it doesn't shadow the model picker
-          ENABLE_OLLAMA_API = "false";
+          ENABLE_OLLAMA_API = "true";
           # Require authentication (first registered user becomes admin)
           WEBUI_AUTH = "True";
         };
