a068c8f9c9628a4ae983dd500cf4c5eed66eeb0b
janusz-bit <janusz-bit@proton.me>
2026-05-30 08:38:00 +0200

    rpi4/open-webui: enable Ollama with local endpoint

--- modules/hosts/raspberry-pi-4/open-webui.nix
+++ modules/hosts/raspberry-pi-4/open-webui.nix
@@ -10,7 +10,8 @@
         environment = {
           # Point to Hermes Agent API Server (OpenAI-compatible)
           OPENAI_API_BASE_URL = "http://127.0.0.1:8642/v1";
-          # Disable Ollama so it doesn't shadow the model picker
+          # Enable Ollama
+          OLLAMA_BASE_URL = "http://127.0.0.1:11434";
           ENABLE_OLLAMA_API = "true";
           # Require authentication (first registered user becomes admin)
           WEBUI_AUTH = "True";
