2e737cd57833354bd2f95e16cc13959e31c9e1a0
janusz-bit <janusz-bit@proton.me>
2026-06-22 03:52:51 +0200

    nixos: add ffmpeg and extra Java runtimes to Prism Launcher

--- modules/hosts/nixos/packages.nix
+++ modules/hosts/nixos/packages.nix
@@ -22,7 +22,18 @@
         kdePackages.qrca
         signal-desktop
         element-desktop
-        prismlauncher
+        (prismlauncher.override {
+          # Add binary required by some mod
+          additionalPrograms = [ ffmpeg ];
+
+          # Change Java runtimes available to Prism Launcher
+          jdks = [
+            graalvmPackages.graalvm-ce
+            zulu8
+            zulu17
+            zulu
+          ];
+        })
         # lutris
         bootdev-cli
         kdePackages.kcalc
