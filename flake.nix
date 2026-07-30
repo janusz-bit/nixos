4c1596dccbeea87703bf7605c475ebcc6f182986
janusz-bit <janusz-bit@proton.me>
2026-07-30 02:40:41 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-26.05";
     import-tree.url = "github:vic/import-tree";
     flake-parts.url = "github:hercules-ci/flake-parts";
     home-manager = {
