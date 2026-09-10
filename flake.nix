77430724f608baba9ae05cfd986077abfacc0efd
janusz-bit <janusz-bit@proton.me>
2026-09-10 08:18:41 +0200

    flake: drop unused nixpkgs-stable input

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-26.05";
     import-tree.url = "github:vic/import-tree";
     flake-parts.url = "github:hercules-ci/flake-parts";
     home-manager = {
