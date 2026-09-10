54a7a17c70ea46ef642c930cdcf2b44b21c07c91
janusz-bit <janusz-bit@proton.me>
2026-09-10 08:19:26 +0200

    ci: cachyos-kernel-update updates nix-cachyos-kernel input

--- modules/github-actions.nix
+++ modules/github-actions.nix
@@ -49,8 +49,8 @@
             inherit runsOn;
             steps = mkBaseSteps ++ [
               {
-                name = "Update chaotic flake input";
-                run = "nix flake lock --update-input chaotic";
+                name = "Update nix-cachyos-kernel flake input";
+                run = "nix flake lock --update-input nix-cachyos-kernel";
               }
               {
                 name = "Build Kernel";
@@ -62,7 +62,7 @@
                   git config user.name "github-actions[bot]"
                   git config user.email "github-actions[bot]@users.noreply.github.com"
                   git add flake.lock
-                  git diff --cached --quiet || git commit -m "flake.lock: update chaotic"
+                  git diff --cached --quiet || git commit -m "flake.lock: update nix-cachyos-kernel"
                   git push
                 '';
               }
